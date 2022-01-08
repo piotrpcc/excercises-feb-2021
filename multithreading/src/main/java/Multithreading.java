4819768572baa7de8e6f21498bd748a7a5812557
damianTrybuszek <damian.trybuszek@gmail.com>
2022-01-08 11:28:43 +0100

    multithreading works

--- multithreading/src/main/java/Multithreading.java
+++ multithreading/src/main/java/Multithreading.java
@@ -1,9 +1,12 @@
 public class Multithreading
 {
     static int counter = 0;
-    public static void main( String[] args )
-    {
-        Runnable runnable = () -> counter++;
+    public static void main( String[] args ) throws InterruptedException {
+        Runnable runnable = () ->{
+                for (int i=0;i < 10000000; i++) {
+                    counter++;
+                }
+    };
 
         Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
@@ -13,15 +16,13 @@ public class Multithreading
         thread2.start();
         thread3.start();
 
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
         System.out.println(counter);
         System.out.println(counter);
         System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
-        System.out.println(counter);
+
     }
 }
